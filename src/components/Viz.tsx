b9dbd1b04b02bbf4b64b519df5670cee9b6e84d3
Aarni Koskela <akx@iki.fi>
2021-11-27 14:55:45 +0200

    Refactor Viz a bit

--- src/components/Viz.tsx
+++ src/components/Viz.tsx
@@ -2,46 +2,55 @@ import React from "react";
 import { getArrMax } from "../utils";
 import { useInterval } from "react-use";
 
+function drawSpectrum(ctx: CanvasRenderingContext2D, vizNode: AnalyserNode) {
+  const { width, height } = ctx.canvas;
+  const freqArr = new Uint8Array(256);
+  vizNode.getByteFrequencyData(freqArr);
+  ctx.beginPath();
+  ctx.moveTo(0, height);
+  const freqMax = getArrMax(freqArr);
+  for (let i = 0; i < freqArr.length; i++) {
+    const x = (i / freqArr.length) * width;
+    const y = (freqArr[i] / freqMax) * height;
+    ctx.lineTo(x, height - y);
+  }
+  ctx.lineTo(width, height);
+  ctx.lineTo(0, height);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function drawWaveform(ctx: CanvasRenderingContext2D, vizNode: AnalyserNode) {
+  const waveArr = new Float32Array(512);
+  const { width, height } = ctx.canvas;
+  const halfHeight = height / 2;
+  vizNode.getFloatTimeDomainData(waveArr);
+  const waveMax = getArrMax(waveArr);
+  ctx.beginPath();
+  ctx.strokeStyle = "1px solid black";
+  for (let i = 0; i < waveArr.length; i++) {
+    const x = (i / waveArr.length) * width;
+    const y = halfHeight + (waveArr[i] / waveMax) * halfHeight;
+    if (i === 0) {
+      ctx.moveTo(x, y);
+    } else {
+      ctx.lineTo(x, y);
+    }
+  }
+  ctx.stroke();
+}
+
 export function Viz({ vizNode }: { vizNode: AnalyserNode }) {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
   const update = React.useCallback(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-    const waveArr = new Float32Array(512);
-    const freqArr = new Uint8Array(256);
-    vizNode.getFloatTimeDomainData(waveArr);
-    vizNode.getByteFrequencyData(freqArr);
-    const ctx = canvas.getContext("2d")!;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
     canvas.width = +canvas.width;
-    const { width, height } = canvas;
-    const halfHeight = height / 2;
-    const waveMax = getArrMax(waveArr);
-    ctx.beginPath();
-    ctx.strokeStyle = "1px solid black";
-    for (let i = 0; i < waveArr.length; i++) {
-      const x = (i / waveArr.length) * width;
-      const y = halfHeight + (waveArr[i] / waveMax) * halfHeight;
-      if (i === 0) {
-        ctx.moveTo(x, y);
-      } else {
-        ctx.lineTo(x, y);
-      }
-    }
-    ctx.stroke();
-    ctx.fillStyle = "orangered";
-    ctx.globalAlpha = 0.6;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const freqMax = getArrMax(freqArr);
-    for (let i = 0; i < freqArr.length; i++) {
-      const x = (i / freqArr.length) * width;
-      const y = (freqArr[i] / freqMax) * height;
-      ctx.lineTo(x, height - y);
-    }
-    ctx.lineTo(width, height);
-    ctx.lineTo(0, height);
-    ctx.closePath();
-    ctx.fill();
+    ctx.fillStyle = `hsl(${(+new Date() / 100) % 360}, 100%, 60%)`;
+    drawSpectrum(ctx, vizNode);
+    drawWaveform(ctx, vizNode);
   }, [vizNode]);
   useInterval(update, 30);
   return (
